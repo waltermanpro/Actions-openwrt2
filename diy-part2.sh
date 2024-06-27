13e15d166a7235e406d4f4a69832347471269cce
waltermanpro <164705679+waltermanpro@users.noreply.github.com>
2024-06-27 11:30:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,7 +42,7 @@ rm -rf package/luci-app-amlogic
 rm -rf package/lean/luci-app-adguardhome
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/pymumu/smartdns feeds/packages/net/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+rm -rf package/feeds/packages/net/smartdns
+rm -rf package/feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/smartdns package/feeds/packages/net/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns package/feeds/luci/applications/luci-app-smartdns
